4db1c29f941606b4aea7c4add911b6cb43b9877f
Nick Spriet <nick.spriet@student.howest.be>
2015-11-17 20:44:17 +0100

    2de poging commit CollapsingToolbars RideDetailsFragment

--- app/src/main/java/com/howest/nmct/bob/fragments/RideDetailsFragment.java
+++ app/src/main/java/com/howest/nmct/bob/fragments/RideDetailsFragment.java
@@ -1,9 +1,6 @@
 package com.howest.nmct.bob.fragments;
 
-<<<<<<< HEAD
 import android.net.Uri;
-=======
->>>>>>> origin/master
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
